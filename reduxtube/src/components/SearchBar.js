44e3f2fb80989fd700c03decd991273ebd8f3ba0
pedroaniceto <pedro_madri@hotmail.com>
2018-11-17 13:01:31 -0200

    .

---
+++ reduxtube/src/components/SearchBar.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react'
+import {Segment, Input} from 'semantic-ui-react';
+
+
+class SearchBar extends Component {
+    
+    pesquisaTermo = e => {
+        if(e.keyCode === 13) {
+            const termo = e.target.value;
+            console.log(termo);
+        }
+    }
+    render() {
+        return (
+            <div className='search-bar'>
+                <Segment stacket>
+                    <Input icon='search'
+                    onKeyDown={(e) => this.pesquisaTermo(e)}
+                    size='large' 
+                    placeholder='Search for...'></Input>
+                </Segment>
+            </div>
+        )
+    }
+}
+
+export default SearchBar;
